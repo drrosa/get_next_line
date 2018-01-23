16da357085cac7aaa63878aa108b087dfd0771fb
David Rosa Tamsen <rosatamsen@gmail.com>
2018-01-23 10:59:44 -0800

    Add author file and fixed incorrect include

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <get_next_line.h>
+#include "get_next_line.h"
 
 int		ft_file_len(const int fd)
 {
